1db80a96d85202923d4a2ced5fa833bda6782fb3
ochab.mat@gmail.com <ochab.mat@gmail.com>
2024-08-12 20:02:00 +0200

    initial commit

---
+++ src/main/java/com/pregenmed/pgmsignup/PgmSignupApplication.java
@@ -0,0 +1,13 @@
+package com.pregenmed.pgmsignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgmSignupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgmSignupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pregenmed/pgmsignup/PgmSignupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pregenmed.pgmsignup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgmSignupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
